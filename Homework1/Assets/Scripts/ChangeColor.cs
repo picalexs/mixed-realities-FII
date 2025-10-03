cedd75d5744ee2b9736ed7935ccb93b6c0ffcbf2
picalexs <picalexs@gmail.com>
2025-10-03 21:50:54 +0300

    test

---
+++ Homework1/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private readonly float _interval = 2f;
+    private float _timer;
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        Debug.Log("ChangeColor Awake");
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer < _interval) return;
+        _timer = 0f;
+
+        if (_renderer != null)
+        {
+            Debug.Log("Changing color");
+            _renderer.material.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
