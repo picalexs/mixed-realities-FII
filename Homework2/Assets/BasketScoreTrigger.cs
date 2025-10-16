fc78248215d549b23497cdc73a780a13fa9e1113
dar1acraciun <daria.craciun411@gmail.com>
2025-10-16 15:47:38 +0300

    fix score trigger

--- Homework2/Assets/BasketScoreTrigger.cs
+++ Homework2/Assets/BasketScoreTrigger.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class BasketScoreTrigger : MonoBehaviour
 {
     public int points = 2;
 
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Ball"))
+        if (!other.CompareTag("Ball")) return;
+
+        // verificăm dacă mingea e ținută de cineva
+        var grab = other.GetComponent<XRGrabInteractable>();
+        if (grab != null && grab.isSelected)
         {
-            Debug.Log($"🏀 Coș! +{points} puncte");
-            ScoreManager.Instance?.AddScore(points);
+            Debug.Log(" Mingea e ținută — nu contăm scorul.");
+            return;
         }
+
+        // dacă mingea e liberă, dăm puncte
+        Debug.Log($"Coș valid! +{points} puncte");
+        ScoreManager.Instance?.AddScore(points);
     }
 }
